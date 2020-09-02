02f41661ba3cc1a7d99f0a76096f0536b7c2d555
Michael Furrer <mickyfurrer@outlook.com>
2020-09-02 07:26:53 +0200

    primes

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,41 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn calc(num1: i32, num2: i32) -> i32 {
+    let res = num1 * num2;
+    res
+}
+
+#[wasm_bindgen]
+pub fn primes() -> i32 {
+    let mut n = 2;
+    let mut p = Vec::new();
+    p.push(2);
+
+    while n < 10_000_000 {
+      let mut prime = true;
+      let mut i = 0;
+      while p[i] * p[i] <= n {
+        if n % p[i] == 0 {
+          prime = false;
+          break;
+        }
+        i += 1;
+      }
+      if prime {
+        p.push(n);
+      }
+      n += 1;
     }
+    n
 }
